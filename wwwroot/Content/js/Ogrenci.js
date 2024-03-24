dedefd85f91b204b2e1ca30c9c5d55622a37a254
Yuro <110335613+1Yuro@users.noreply.github.com>
2024-03-25 00:44:11 +0300

    Delete wwwroot directory

--- wwwroot/Content/js/Ogrenci.js
+++
@@ -1,5 +0,0 @@
-﻿function changeIframeSrc(controller, action, parameter) {
-    var iframe = document.getElementById("myIframe");
-    var newSrc = '/' + controller + '/' + action + '?ogrenciid=' + parameter;
-    iframe.src = newSrc;
-}
